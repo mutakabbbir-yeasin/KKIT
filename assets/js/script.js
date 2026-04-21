699be8388e0f2354d8370265505c4547925a45b9
Mutakabbir Yeasin <mutakabbir.yeasin@gmail.com>
2026-04-21 22:15:25 +0600

    testimonial section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 
 AOS.init({ duration: 500, once: true });
     
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  speed: 800,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    }
+  }
+});
