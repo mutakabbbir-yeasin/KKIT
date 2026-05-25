e22c9e182e417413c7fe4d92f9c37b5c76862d08
Mutakabbir Yeasin <mutakabbir.yeasin@gmail.com>
2026-05-26 00:29:18 +0600

    contact page done and about us page half done

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,55 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });
+
+
+/*=================== Contact Page ==================*/
+ const form = document.getElementById("contactForm");
+        const successMessage = document.getElementById("successMessage");
+
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            successMessage.style.display = "block";
+
+            form.reset();
+
+            setTimeout(() => {
+                successMessage.style.display = "none";
+            }, 4000);
+        });
+
+
+/*=============== ABOUT US PAGE  ===============*/
+// Counter Animation
+
+        const counters = document.querySelectorAll('.counter');
+
+        counters.forEach(counter => {
+
+            counter.innerText = '0';
+
+            const updateCounter = () => {
+
+                const target = +counter.getAttribute('data-target');
+                const current = +counter.innerText;
+
+                const increment = target / 100;
+
+                if (current < target) {
+
+                    counter.innerText = `${Math.ceil(current + increment)}`;
+
+                    setTimeout(updateCounter, 20);
+
+                } else {
+
+                    counter.innerText = target;
+
+                }
+
+            };
+
+            updateCounter();
+
+        });
